8e27a305b55ce3be4e9e9f6ab02a20e9222aa06e
fdl <1101623201@qq.com>
2023-11-16 23:48:04 +0800

    [si.li] #005 feature: add get current admin user api

--- spzx-admin/src/main/java/com/atguigu/spzx/admin/controller/IndexController.java
+++ spzx-admin/src/main/java/com/atguigu/spzx/admin/controller/IndexController.java
@@ -3,11 +3,14 @@ package com.atguigu.spzx.admin.controller;
 
 import com.atguigu.spzx.admin.service.IndexService;
 import com.atguigu.spzx.model.dto.system.LoginDto;
+import com.atguigu.spzx.model.entity.system.SysUser;
 import com.atguigu.spzx.model.vo.common.Result;
 import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
 import com.atguigu.spzx.model.vo.system.LoginVo;
 import com.atguigu.spzx.model.vo.system.CaptchaVo;
+import com.atguigu.spzx.utils.AuthContextUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +32,10 @@ public class IndexController {
   public Result<CaptchaVo> captcha() {
     CaptchaVo captchaVo = indexService.generateCaptcha();
     return Result.build(captchaVo, ResultCodeEnum.SUCCESS);
+  }
+
+  @GetMapping("/currentUser")
+  public Result<SysUser> currentUser() {
+    return Result.build(AuthContextUtil.getAdminUser(), ResultCodeEnum.SUCCESS);
   }
 }
