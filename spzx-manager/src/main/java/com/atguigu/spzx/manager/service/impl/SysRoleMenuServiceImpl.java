79ab2fcbb85361ab5e4c19693e9bcffe401aaef3
fubo.ding <fubo.ding@thoughtworks.com>
2023-11-24 16:00:57 +0800

    [san.zhang] #012 refactor: optimize /admin/system/sysRoleMenu apis

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysRoleMenuServiceImpl.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysRoleMenuServiceImpl.java
@@ -5,47 +5,50 @@ import com.atguigu.spzx.manager.service.SysMenuService;
 import com.atguigu.spzx.manager.service.SysRoleMenuService;
 import com.atguigu.spzx.model.dto.system.AssginMenuDto;
 import com.atguigu.spzx.model.entity.system.SysMenu;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SysRoleMenuServiceImpl implements SysRoleMenuService {
 
-    @Autowired
-    private SysRoleMenuMapper sysRoleMenuMapper;
+  @Autowired
+  private SysRoleMenuMapper sysRoleMenuMapper;
 
-    @Autowired
-    private SysMenuService sysMenuService;
+  @Autowired
+  private SysMenuService sysMenuService;
 
-    //1 查询所有菜单 和 查询角色分配过菜单id列表
-    @Override
-    public Map<String, Object> findSysRoleMenuByRoleId(Long roleId) {
-        // 查询所有菜单
-        List<SysMenu> sysMenuList = sysMenuService.findNodes();
+  //1 查询所有菜单 和 查询角色分配过菜单id列表
+  @Override
+  public Map<String, Object> findSysRoleMenuByRoleId(Long roleId) {
+    // 查询所有菜单
+    List<SysMenu> sysMenuList = sysMenuService.findNodes();
 
-        // 询角色分配过菜单id列表
-        List<Long> roleMenuIds = sysRoleMenuMapper.findSysRoleMenuByRoleId(roleId);
+    // 询角色分配过菜单id列表
+    List<Long> roleMenuIds = sysRoleMenuMapper.findSysRoleMenuByRoleId(roleId);
 
-        Map<String, Object> map = new HashMap<>();
-        map.put("sysMenuList",sysMenuList);
-        map.put("roleMenuIds",roleMenuIds);
-        return map;
+    Map<String, Object> map = new HashMap<>();
+    map.put("sysMenuList", sysMenuList);
+    map.put("roleMenuIds", roleMenuIds);
+    return map;
+  }
+
+  //分配菜单
+  @Override
+  @Transactional
+  public void doAssign(AssginMenuDto assginMenuDto) {
+    //删除角色分配菜单数据
+    sysRoleMenuMapper.deleteByRoleId(assginMenuDto.getRoleId());
+
+    //保存分配数据
+    List<Map<String, Number>> menuInfo = assginMenuDto.getMenuIdList();
+    if (CollectionUtils.isNotEmpty(menuInfo)) { //角色分配了菜单
+      sysRoleMenuMapper.doAssign(assginMenuDto);
     }
-
-    //分配菜单
-    @Override
-    public void doAssign(AssginMenuDto assginMenuDto) {
-        //删除角色分配菜单数据
-        sysRoleMenuMapper.deleteByRoleId(assginMenuDto.getRoleId());
-
-        //保存分配数据
-        List<Map<String, Number>> menuInfo = assginMenuDto.getMenuIdList();
-        if(menuInfo != null && menuInfo.size()>0) { //角色分配了菜单
-            sysRoleMenuMapper.doAssign(assginMenuDto);
-        }
-    }
+  }
 }
