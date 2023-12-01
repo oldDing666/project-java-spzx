94f4186bae100670518f29592faa7dd23af2c9c3
fubo.ding <fubo.ding@thoughtworks.com>
2023-12-01 15:16:58 +0800

    [san.zhang] #027 refactor: optimize /api/order/cart api

--- spzx-service/service-cart/src/main/java/com/atguigu/spzx/cart/controller/CartController.java
+++ spzx-service/service-cart/src/main/java/com/atguigu/spzx/cart/controller/CartController.java
@@ -15,68 +15,72 @@ import java.util.List;
 @RequestMapping("/api/order/cart")
 public class CartController {
 
-    @Autowired
-    private CartService cartService;
+  @Autowired
+  private CartService cartService;
 
-    @Operation(summary="清空购物车")
-    @GetMapping("/auth/clearCart")
-    public Result clearCart(){
-        cartService.clearCart();
-        return Result.build(null, ResultCodeEnum.SUCCESS);
-    }
+  @Operation(summary = "清空购物车")
+  @GetMapping("/auth/clearCart")
+  public Result<?> clearCart() {
+    cartService.clearCart();
+    return Result.build(null, ResultCodeEnum.SUCCESS);
+  }
 
-    @Operation(summary="更新购物车商品全部选中状态")
-    @GetMapping("/auth/allCheckCart/{isChecked}")
-    public Result allCheckCart(@Parameter(name = "isChecked", description = "是否选中 1:选中 0:取消选中", required = true) @PathVariable(value = "isChecked") Integer isChecked){
-        cartService.allCheckCart(isChecked);
-        return Result.build(null, ResultCodeEnum.SUCCESS);
-    }
+  @Operation(summary = "更新购物车商品全部选中状态")
+  @GetMapping("/auth/allCheckCart/{isChecked}")
+  public Result<?> allCheckCart(
+      @Parameter(name = "isChecked", description = "是否选中 1:选中 0:取消选中", required = true)
+      @PathVariable(value = "isChecked") Integer isChecked) {
+    cartService.allCheckCart(isChecked);
+    return Result.build(null, ResultCodeEnum.SUCCESS);
+  }
 
-    @Operation(summary="更新购物车商品选中状态")
-    @GetMapping("/auth/checkCart/{skuId}/{isChecked}")
-    public Result checkCart(@Parameter(name = "skuId", description = "商品skuId", required = true) @PathVariable(value = "skuId") Long skuId,
-                            @Parameter(name = "isChecked", description = "是否选中 1:选中 0:取消选中", required = true) @PathVariable(value = "isChecked") Integer isChecked) {
-        cartService.checkCart(skuId,isChecked);
-        return Result.build(null,ResultCodeEnum.SUCCESS);
-    }
+  @Operation(summary = "更新购物车商品选中状态")
+  @GetMapping("/auth/checkCart/{skuId}/{isChecked}")
+  public Result<?> checkCart(
+      @Parameter(name = "skuId", description = "商品skuId", required = true)
+      @PathVariable(value = "skuId") Long skuId,
+      @Parameter(name = "isChecked", description = "是否选中 1:选中 0:取消选中", required = true)
+      @PathVariable(value = "isChecked") Integer isChecked) {
+    cartService.checkCart(skuId, isChecked);
+    return Result.build(null, ResultCodeEnum.SUCCESS);
+  }
 
-    @Operation(summary = "删除购物车商品")
-    @DeleteMapping("auth/deleteCart/{skuId}")
-    public Result deleteCart(@PathVariable("skuId") Long skuId) {
-        cartService.deleteCart(skuId);
-        return Result.build(null,ResultCodeEnum.SUCCESS);
-    }
+  @Operation(summary = "删除购物车商品")
+  @DeleteMapping("/auth/deleteCart/{skuId}")
+  public Result<?> deleteCart(@PathVariable("skuId") Long skuId) {
+    cartService.deleteCart(skuId);
+    return Result.build(null, ResultCodeEnum.SUCCESS);
+  }
 
-    @Operation(summary = "查询购物车")
-    @GetMapping("auth/cartList")
-    public Result cartList() {
-        List<CartInfo> cartInfoList = cartService.getCartList();
-        return Result.build(cartInfoList,ResultCodeEnum.SUCCESS);
-    }
+  @Operation(summary = "查询购物车")
+  @GetMapping("/auth/cartList")
+  public Result<List<CartInfo>> cartList() {
+    List<CartInfo> cartInfoList = cartService.getCartList();
+    return Result.build(cartInfoList, ResultCodeEnum.SUCCESS);
+  }
 
-    //skuId:商品sku的id值
-    //skuNum：商品数量
-    @Operation(summary = "添加购物车")
-    @GetMapping("auth/addToCart/{skuId}/{skuNum}")
-    public Result addToCart(@PathVariable Long skuId,
-                            @PathVariable Integer skuNum) {
-        cartService.addToCart(skuId,skuNum);
-        return Result.build(null, ResultCodeEnum.SUCCESS);
-    }
+  //skuId:商品sku的id值
+  //skuNum：商品数量
+  @Operation(summary = "添加购物车")
+  @GetMapping("/auth/addToCart/{skuId}/{skuNum}")
+  public Result<?> addToCart(@PathVariable Long skuId,
+      @PathVariable Integer skuNum) {
+    cartService.addToCart(skuId, skuNum);
+    return Result.build(null, ResultCodeEnum.SUCCESS);
+  }
 
-    //远程调用：订单结算使用，获取购物车选中商品列表
-    @Operation(summary="选中的购物车")
-    @GetMapping(value = "/auth/getAllCkecked")
-    public List<CartInfo> getAllCkecked() {
-        List<CartInfo> list = cartService.getAllCkecked();
-        return list;
-    }
+  //远程调用：订单结算使用，获取购物车选中商品列表
+  @Operation(summary = "选中的购物车")
+  @GetMapping(value = "/auth/getAllCkecked")
+  public List<CartInfo> getAllCkecked() {
+    return cartService.getAllChecked();
+  }
 
-    //远程调用：删除生成订单的购物车商品
-    @GetMapping("/auth/deleteChecked")
-    public Result deleteChecked() {
-        cartService.deleteChecked();
-        return Result.build(null, ResultCodeEnum.SUCCESS);
-    }
+  //远程调用：删除生成订单的购物车商品
+  @GetMapping("/auth/deleteChecked")
+  public Result<?> deleteChecked() {
+    cartService.deleteChecked();
+    return Result.build(null, ResultCodeEnum.SUCCESS);
+  }
 
 }
--- spzx-service/service-cart/src/main/java/com/atguigu/spzx/cart/service/CartService.java
+++ spzx-service/service-cart/src/main/java/com/atguigu/spzx/cart/service/CartService.java
@@ -6,27 +6,27 @@ import java.util.List;
 
 public interface CartService {
 
-    //添加购物车
-    void addToCart(Long skuId, Integer skuNum);
+  //添加购物车
+  void addToCart(Long skuId, Integer skuNum);
 
-    //查询购物车
-    List<CartInfo> getCartList();
+  //查询购物车
+  List<CartInfo> getCartList();
 
-    //删除购物车商品
-    void deleteCart(Long skuId);
+  //删除购物车商品
+  void deleteCart(Long skuId);
 
-    //更新购物车商品选中状态
-    void checkCart(Long skuId, Integer isChecked);
+  //更新购物车商品选中状态
+  void checkCart(Long skuId, Integer isChecked);
 
-    //更新购物车商品全部选中状态
-    void allCheckCart(Integer isChecked);
+  //更新购物车商品全部选中状态
+  void allCheckCart(Integer isChecked);
 
-    //清空购物车
-    void clearCart();
+  //清空购物车
+  void clearCart();
 
-    //远程调用：订单结算使用，获取购物车选中商品列表
-    List<CartInfo> getAllCkecked();
+  //远程调用：订单结算使用，获取购物车选中商品列表
+  List<CartInfo> getAllChecked();
 
-    //远程调用：删除生成订单的购物车商品
-    void deleteChecked();
+  //远程调用：删除生成订单的购物车商品
+  void deleteChecked();
 }
