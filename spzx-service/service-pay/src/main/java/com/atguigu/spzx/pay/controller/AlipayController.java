2e20bdce6a4c980cce8a5c8f4a1679fbff17a680
fubo.ding <fubo.ding@thoughtworks.com>
2023-12-01 18:06:50 +0800

    [san.zhang] #029 refactor: optimize /api/order/alipay api

--- spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/controller/AlipayController.java
+++ spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/controller/AlipayController.java
@@ -8,8 +8,6 @@ import com.atguigu.spzx.pay.service.AlipayService;
 import com.atguigu.spzx.pay.service.PaymentInfoService;
 import com.atguigu.spzx.pay.utils.AlipayProperties;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -20,49 +18,47 @@ import java.util.Map;
 @RequestMapping("/api/order/alipay")
 public class AlipayController {
 
-    @Autowired
-    private AlipayService alipayService;
+  @Autowired
+  private AlipayService alipayService;
 
-    @Autowired
-    private AlipayProperties alipayProperties;
+  @Autowired
+  private AlipayProperties alipayProperties;
 
-    @Autowired
-    private PaymentInfoService paymentInfoService;
+  @Autowired
+  private PaymentInfoService paymentInfoService;
 
-    @Operation(summary="支付宝异步回调")
-    @RequestMapping("callback/notify")
-    @ResponseBody
-    public String alipayNotify(@RequestParam Map<String, String> paramMap, HttpServletRequest request) {
-        //签名校验
-        boolean signVerified = false; //调用SDK验证签名
-        try {
-            signVerified = AlipaySignature.rsaCheckV1(paramMap, alipayProperties.getAlipayPublicKey(), AlipayProperties.charset, AlipayProperties.sign_type);
-        } catch (AlipayApiException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("********"+paramMap);
-        String trade_status = paramMap.get("trade_status");
-
-        if(signVerified) {
-            if ("TRADE_SUCCESS".equals(trade_status) || "TRADE_FINISHED".equals(trade_status)) {
-                // 正常的支付成功，我们应该更新交易记录状态
-                paymentInfoService.updatePaymentStatus(paramMap);
-                return "success";
-            }
-
-        } else {
-            return "failure";
-        }
-        return "failure";
+  @Operation(summary = "支付宝异步回调")
+  @RequestMapping("/callback/notify")
+  @ResponseBody
+  public String alipayNotify(@RequestParam Map<String, String> paramMap) {
+    //签名校验
+    boolean signVerified = false; //调用SDK验证签名
+    try {
+      signVerified = AlipaySignature.rsaCheckV1(paramMap,
+          alipayProperties.getAlipayPublicKey(),
+          AlipayProperties.charset,
+          AlipayProperties.sign_type);
+    } catch (AlipayApiException e) {
+      e.printStackTrace();
     }
 
-    //支付宝下单
-    @GetMapping("submitAlipay/{orderNo}")
-    @ResponseBody
-    public Result submitAlipay(@PathVariable(value = "orderNo") String orderNo) {
-        String form = alipayService.submitAlipay(orderNo);
-        return Result.build(form, ResultCodeEnum.SUCCESS);
+    System.out.println("********" + paramMap);
+    String trade_status = paramMap.get("trade_status");
+
+    if (signVerified && ("TRADE_SUCCESS".equals(trade_status) || "TRADE_FINISHED".equals(trade_status))) {
+      // 正常的支付成功，我们应该更新交易记录状态
+      paymentInfoService.updatePaymentStatus(paramMap);
+      return "success";
     }
+    return "failure";
+  }
+
+  //支付宝下单
+  @GetMapping("/submitAlipay/{orderNo}")
+  @ResponseBody
+  public Result<String> submitAlipay(@PathVariable(value = "orderNo") String orderNo) {
+    String form = alipayService.submitAlipay(orderNo);
+    return Result.build(form, ResultCodeEnum.SUCCESS);
+  }
 
 }
--- spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/service/AlipayService.java
+++ spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/service/AlipayService.java
@@ -2,6 +2,6 @@ package com.atguigu.spzx.pay.service;
 
 public interface AlipayService {
 
-    //支付宝下单
-    String submitAlipay(String orderNo);
+  //支付宝下单
+  String submitAlipay(String orderNo);
 }
